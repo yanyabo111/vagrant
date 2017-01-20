194b9a4d0924fd0738cd1a5456de4bde5eff4546
yanyabo111 <yanyabo111@gmail.com>
2017-01-21 01:05:24 +0800

    add .spacemacs file, add some init setting for emacs

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -49,3 +49,10 @@
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+
+
+; custom setup
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(split-window-right)
+(neotree-find-project-root)
+
